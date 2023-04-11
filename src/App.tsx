658619d2e09713c17a038c26ae0e897f0977da04
nrsimonelli <nrsimonelli@gmail.com>
2023-04-11 17:48:46 -0500

    fix(CSS): additional styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import './App.css'
 
 export const App = () => {
   return (
-    <div className='App'>
+    <div>
       {/* Heading */}
       <h1>Fire Emblem Engage</h1>
       <h3>Choose what you would like to compare below</h3>
       {/* Toggle here */}
-      <p className='PromptText'>Select your option and click Add</p>
-      <div>
+      <p className='prompt-text'>Select your option and click Add</p>
+      <div className='option-row'>
         {/* Input fields here here */}
-        <button className='button'>Add</button>
+        <button className='add button'>Add</button>
         <button className='button'>Clear</button>
       </div>
     </div>
