1df3cc15a7ae3f6d53e5cca3332a7e513497754d
nrsimonelli <nrsimonelli@gmail.com>
2023-04-11 17:23:15 -0500

    feat(STYLE): basic org

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <div className='App'></div>
-}
+import './App.css'
 
-export default App
+export const App = () => {
+  return (
+    <div className='App'>
+      {/* Heading */}
+      <h1>Fire Emblem Engage</h1>
+      <h3>Choose what you would like to compare below</h3>
+      {/* Toggle here */}
+      <p className='PromptText'>Select your option and click Add</p>
+      <div>
+        {/* Input fields here here */}
+        <button className='button'>Add</button>
+        <button className='button'>Clear</button>
+      </div>
+    </div>
+  )
+}
