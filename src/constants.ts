65e3cc3b63a2f131fbfacf0787b6a8b60b8afe3f
nrsimonelli <nrsimonelli@gmail.com>
2023-04-12 15:08:23 -0500

    feat(CONST): added job groupings

--- src/constants.ts
+++ src/constants.ts
@@ -103,3 +103,189 @@ export const JOB_NAME = {
   WOLF_KNIGHT: 'Wolf Knight',
   WYVERN_KNIGHT: 'Wyvern Knight',
 }
+
+export const JOB_GROUP = {
+  ADVANCED: {
+    AVENIR: {
+      isExclusive: UNIT_NAME.ALFRED,
+    },
+    BERSERKER: {
+      isExclusive: false,
+    },
+    BOW_KNIGHT: {
+      isExclusive: false,
+    },
+    CUPIDO: {
+      isExclusive: UNIT_NAME.FOGADO,
+    },
+    DIVINE_DRAGON: {
+      isExclusive: UNIT_NAME.ALEAR,
+    },
+    ENCHANTER: {
+      isExclusive: false,
+    },
+    GENERAL: {
+      isExclusive: false,
+    },
+    GREAT_KNIGHT: {
+      isExclusive: false,
+    },
+    GRIFFIN_KNIGHT: {
+      isExclusive: false,
+    },
+    HALBERDIER: {
+      isExclusive: false,
+    },
+    HERO: {
+      isExclusive: false,
+    },
+    HIGH_PRIEST: {
+      isExclusive: false,
+    },
+    LINDWURM: {
+      isExclusive: UNIT_NAME.IVY,
+    },
+    MAGE_CANNONEER: {
+      isExclusive: false,
+    },
+    MAGE_KNIGHT: {
+      isExclusive: false,
+    },
+    MARTIAL_MASTER: {
+      isExclusive: false,
+    },
+    PALADIN: {
+      isExclusive: false,
+    },
+    PICKET: {
+      isExclusive: UNIT_NAME.TIMERRA,
+    },
+    ROYAL_KNIGHT: {
+      isExclusive: false,
+    },
+    SAGE: {
+      isExclusive: false,
+    },
+    SLEIPNIR_RIDER: {
+      isExclusive: UNIT_NAME.HORTENSIA,
+    },
+    SNIPER: {
+      isExclusive: false,
+    },
+    SUCCESSEUR: {
+      isExclusive: UNIT_NAME.DIAMANT,
+    },
+    SWORDMASTER: {
+      isExclusive: false,
+    },
+    TIREUR_DELITE: {
+      isExclusive: UNIT_NAME.ALCRYST,
+    },
+    VIDAME: {
+      isExclusive: UNIT_NAME.CELINE,
+    },
+    WARRIOR: {
+      isExclusive: false,
+    },
+    WOLF_KNIGHT: {
+      isExclusive: false,
+    },
+    WYVERN_KNIGHT: {
+      isExclusive: false,
+    },
+  },
+  BASE: {
+    ARCHER: {
+      isExclusive: false,
+    },
+    AXE_ARMOR: {
+      isExclusive: false,
+    },
+    AXE_CAVALIER: {
+      isExclusive: false,
+    },
+    AXE_FIGHTER: {
+      isExclusive: false,
+    },
+    AXE_FLIER: {
+      isExclusive: false,
+    },
+    DRAGON_CHILD: {
+      isExclusive: UNIT_NAME.ALEAR,
+    },
+    LANCE_ARMOR: {
+      isExclusive: false,
+    },
+    LANCE_CAVALIER: {
+      isExclusive: false,
+    },
+    LANCE_FIGHTER: {
+      isExclusive: false,
+    },
+    LANCE_FLIER: {
+      isExclusive: false,
+    },
+    LORD_ALCRYST: {
+      isExclusive: UNIT_NAME.ALCRYST,
+    },
+    LORD_DIAMANT: {
+      isExclusive: UNIT_NAME.DIAMANT,
+    },
+    MAGE: {
+      isExclusive: false,
+    },
+    MARTIAL_MONK: {
+      isExclusive: false,
+    },
+    NOBLE_ALFRED: {
+      isExclusive: UNIT_NAME.ALFRED,
+    },
+    NOBLE_CELINE: {
+      isExclusive: UNIT_NAME.CELINE,
+    },
+    SENTINEL_FOGADO: {
+      isExclusive: UNIT_NAME.FOGADO,
+    },
+    SENTINEL_TIMERRA: {
+      isExclusive: UNIT_NAME.TIMERRA,
+    },
+    SWORD_ARMOR: {
+      isExclusive: false,
+    },
+    SWORD_CAVALIER: {
+      isExclusive: false,
+    },
+    SWORD_FIGHTER: {
+      isExclusive: false,
+    },
+    SWORD_FLIER: {
+      isExclusive: false,
+    },
+    WING_TAMER_HORTENSIA: {
+      isExclusive: UNIT_NAME.HORTENSIA,
+    },
+    WING_TAMER_IVY: {
+      isExclusive: UNIT_NAME.IVY,
+    },
+  },
+  SPECIAL: {
+    DANCER: {
+      isExclusive: UNIT_NAME.SEADALL,
+    },
+    FELL_CHILD: {
+      isExclusive: UNIT_NAME.VEYLE,
+    },
+    FELL_CHILD_NEL: {
+      isExclusive: UNIT_NAME.NEL,
+    },
+    FELL_CHILD_RAFAL: {
+      isExclusive: UNIT_NAME.RAFAL,
+    },
+    MELUSINE: {
+      isExclusive: UNIT_NAME.ZELESTIA,
+    },
+    THIEF: {
+      isExclusive: false,
+    },
+  },
+}
