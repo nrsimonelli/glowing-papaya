01f09a45b56c23410a7ee53e78c100d520b85910
nrsimonelli <nrsimonelli@gmail.com>
2023-04-14 18:22:28 -0500

    feat(UTIL): more fn

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,14 @@ export type CharacterDetail = { unitName: UnitName; jobName: JobName }
 type ValueOf<T> = T[keyof T]
 type Entries<T> = [keyof T, ValueOf<T>][]
 
+export const objectKeys = <T extends object>(obj: T) => {
+  return Object.keys(obj) as (keyof T)[]
+}
+
+export const objectValues = <T extends object>(obj: T) => {
+  return Object.values(obj) as ValueOf<T>[]
+}
+
 export const objectEntries = <T extends object>(obj: T) => {
   return Object.entries(obj) as Entries<T>
 }
