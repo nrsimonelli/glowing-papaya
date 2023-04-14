57c360ab24ff56f612e67eef56506a2223842253
nrsimonelli <nrsimonelli@gmail.com>
2023-04-14 17:36:55 -0500

    feat(SELECT): mapped unit and job names to select

--- src/SelectJob.tsx
+++ src/SelectJob.tsx
@@ -1,14 +1,39 @@
+import { SelectGroup, SelectLabel } from '@radix-ui/react-select'
 import { Select, SelectItem } from './components/Select'
+import { JOB_NAME, JOB_GROUP, UNIT_NAME } from './constants'
+import { UnitName, objectEntries } from './utils'
 
 interface SelectJobProps {
   value: string
   onValueChange: React.Dispatch<React.SetStateAction<string>>
+  selectedUnit: string
 }
-export const SelectJob = ({ value, onValueChange }: SelectJobProps) => {
+export const SelectJob = ({
+  value,
+  onValueChange,
+  selectedUnit,
+}: SelectJobProps) => {
   return (
     <Select value={value} onValueChange={onValueChange}>
       <SelectItem value={'default'}>{'Select a class'}</SelectItem>
-      {/* Map Units... */}
+      {/* Map Jobs... */}
+      {objectEntries(JOB_GROUP).map(([group, jobList]) => (
+        <SelectGroup key={group}>
+          <SelectLabel className={'select-label'}>{group}</SelectLabel>
+          {objectEntries(jobList).map(([key, { isExclusive }]) => {
+            if (
+              !isExclusive ||
+              isExclusive === UNIT_NAME[selectedUnit as UnitName]
+            ) {
+              return (
+                <SelectItem key={key} value={key}>
+                  {JOB_NAME[key]}
+                </SelectItem>
+              )
+            }
+          })}
+        </SelectGroup>
+      ))}
     </Select>
   )
 }
--- src/SelectUnit.tsx
+++ src/SelectUnit.tsx
@@ -1,4 +1,6 @@
 import { Select, SelectItem } from './components/Select'
+import { UNIT_NAME } from './constants'
+import { UnitName, objectEntries } from './utils'
 
 interface SelectUnitProps {
   value: string
@@ -9,6 +11,11 @@ export const SelectUnit = ({ value, onValueChange }: SelectUnitProps) => {
     <Select value={value} onValueChange={onValueChange}>
       <SelectItem value={'default'}>{'Select a character'}</SelectItem>
       {/* Map Units... */}
+      {objectEntries(UNIT_NAME).map(([unitKey, displayName]) => (
+        <SelectItem key={unitKey} value={unitKey}>
+          {displayName}
+        </SelectItem>
+      ))}
     </Select>
   )
 }
