0fb4f9de536e7bf3a8fb75eed724b7424d5ec4d8
nrsimonelli <nrsimonelli@gmail.com>
2023-05-02 17:45:47 -0500

    feat(SLIDER): added component

---
+++ src/components/Slider.tsx
@@ -0,0 +1,26 @@
+import * as SliderPrimitive from '@radix-ui/react-slider'
+import React from 'react'
+
+export const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ ...props }, forwardedRef) => {
+  const value = props.value || props.defaultValue
+
+  return (
+    <SliderPrimitive.Slider
+      className={'slider-root'}
+      {...props}
+      ref={forwardedRef}
+    >
+      <SliderPrimitive.Track className={'slider-track'}>
+        <SliderPrimitive.Range className={'slider-range'} />
+      </SliderPrimitive.Track>
+      {value?.map((_, i) => (
+        <SliderPrimitive.SliderThumb className={'slider-thumb'} key={i}>
+          {props.children}
+        </SliderPrimitive.SliderThumb>
+      ))}
+    </SliderPrimitive.Slider>
+  )
+})
