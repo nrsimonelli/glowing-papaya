8d2a04e182a1cf2aa9c570ae296ac71c3753f9bb
nrsimonelli <nrsimonelli@gmail.com>
2023-05-07 20:30:39 -0500

    reafactor alpha slider WIP

--- src/AlphaSlider.tsx
+++ src/AlphaSlider.tsx
@@ -3,29 +3,45 @@ import { useEffect, useState } from 'react'
 import * as SliderPrimitive from '@radix-ui/react-slider'
 import { JOB_GROUP, JOB_NAME, PERSONAL_BASE, STAT_KEY } from './constants'
 import { useCharacterData } from './utils/useCharacterData'
-import { StatKey, UnitName, objectEntries, objectKeys } from './utils/types'
+import {
+  InitialValues,
+  JobName,
+  StatKey,
+  UnitName,
+  objectEntries,
+  objectKeys,
+  objectValues,
+} from './utils/types'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+// import { Dropdown } from './components/Dropdown'
 
-export const AlphaSlider = ({ unit }: { unit: UnitName }) => {
-  const {
-    LV: baseLv,
-    EXP: baseExp,
-    JOB: baseJob,
-    STATS: baseStats,
-  } = PERSONAL_BASE[unit]
-  const [totalExp, setTotalExp] = useState<number>(baseExp)
-  const [currentJob, setCurrentJob] = useState(baseJob)
-  const [currentStats, setCurrentStats] = useState(baseStats)
+export const AlphaSlider = ({
+  unit,
+}: {
+  unit: UnitName
+  unitHistory: {
+    lv: number
+    exp: number
+    job: JobName
+    stats: InitialValues
+  }[]
+}) => {
+  const { LV, EXP, JOB, STATS } = PERSONAL_BASE[unit]
+  const [baseExp, setBaseExp] = useState(EXP)
+  const [baseLv, setBaseLv] = useState(LV)
+  const [totalExp, setTotalExp] = useState<number>(EXP)
   const [currentLevel, setCurrentLevel] = useState<number>(baseLv)
-
+  const [currentJob, setCurrentJob] = useState<JobName>(JOB)
+  const [currentStats, setCurrentStats] = useState(STATS)
+  const [step, setStep] = useState(1)
   const [unitHistory, setUnitHistory] = useState([
     {
-      lv: baseLv,
-      exp: baseExp,
-      job: baseJob,
-      stats: baseStats,
+      lv: LV,
+      exp: EXP,
+      job: JOB,
+      stats: STATS,
     },
   ])
-
   const { combinedGrowth: growthRate, personalCap: statCap } = useCharacterData(
     unit,
     currentJob
@@ -39,55 +55,103 @@ export const AlphaSlider = ({ unit }: { unit: UnitName }) => {
     (job) => job === currentJob
   )
   const levelCap = isSpecial ? 40 : 20
-  const isLevelCapped = currentLevel === levelCap
-  const canPromote = (level: number) => {
-    if (isAdvanced) {
-      return true
-    }
-    if (isSpecial) {
-      return level >= 20
-    }
-    if (isBase) {
-      return level >= 10
-    }
-    return false
-  }
 
-  const getModifiedStat = (stat, val) => {
-    const modifier =
-      (growthRate[stat as StatKey] / 100) * ((totalExp - baseExp) / 100)
-    return Number(val + modifier).toFixed(1)
+  const getModifiedStat = (stat: StatKey, val: number, lv: number) => {
+    const modifier = (growthRate[stat] / 100) * (lv - currentLevel)
+    // ((Math.floor(totalExp / 100) * 100 - baseExp) / 100)
+    return Number((val + modifier).toFixed(2))
   }
+  const isLevelCapped = currentLevel === levelCap
+  const expCap = (levelCap - baseLv) * 100 + baseExp
+  const updatedLevel = Math.floor((totalExp - baseExp) / 100 + baseLv)
 
   const onValueChange = (val: number[]) => {
-    if (isLevelCapped && val[1] > totalExp) {
+    if (!ready) {
       return
     }
-    if (val[1] !== totalExp) {
+    // if (step < unitHistory.length && val[1] > expCap) {
+    //   stepForward(step + 1)
+    //   return
+    // }
+    if (step > 1 && val[1] < baseExp) {
+      stepBackward(step - 1)
+      return
+    }
+
+    // normal
+    if (val[1] > expCap) {
+      setTotalExp(expCap)
+    } else if (val[1] !== totalExp) {
       setTotalExp(val[1])
+    }
+    const updatedLevel = Math.floor((totalExp - baseExp) / 100 + baseLv)
+    if (currentLevel !== updatedLevel) {
+      console.log('sending to lvChange')
+      handleLvChange(updatedLevel)
     }
   }
 
-  const onValueCommit = () => {}
+  const masterSealOptions = (unit) => {}
 
-  // onLevelChange
-  useEffect(() => {
-    const updatedLevel = Math.floor((totalExp - baseExp) / 100 + baseLv)
-    const expCap = (levelCap - baseLv) * 100 + baseExp
+  const handleLvChange = (lv: number) => {
+    console.log('in LVCHANGE', lv)
+    const updatedStats = { ...currentStats }
+    for (const [stat, value] of objectEntries(updatedStats)) {
+      updatedStats[stat] = getModifiedStat(stat, value, lv)
+    }
+    setCurrentLevel(lv)
+    setCurrentStats(updatedStats)
+  }
 
-    if (updatedLevel > levelCap) {
-      console.log('nice try')
-      setCurrentLevel(levelCap)
-      setTotalExp(expCap)
+  const stepBackward = (prevStep: number) => {
+    const stepData = unitHistory[prevStep - 1]
+    const prevLv = Math.floor((baseExp - 1 - stepData.exp) / 100 + stepData.lv)
+    setStep(prevStep)
+    setTotalExp(baseExp - 1)
+    setBaseExp(stepData.exp)
+    setCurrentLevel(prevLv)
+    setCurrentJob(stepData.job)
+    setBaseLv(stepData.lv)
+    const prevStats = { ...stepData.stats }
+    const { combinedGrowth: prevGrowthRate } = useCharacterData(
+      unit,
+      stepData.job
+    )
+    for (const [stat, val] of objectEntries(prevStats)) {
+      const modifier = (prevGrowthRate[stat] / 100) * (prevLv - stepData.lv)
+      prevStats[stat] = Number((val + modifier).toFixed(2))
     }
-    if (currentLevel !== updatedLevel) {
-      setCurrentLevel(updatedLevel)
+    setCurrentStats(prevStats)
+    setUnitHistory(unitHistory.slice(0, -1))
+  }
+
+  const handleJobChange = (job: JobName) => {
+    const roundedTotalExp = Math.floor(totalExp / 100) * 100
+    setTotalExp(roundedTotalExp)
+    setBaseExp(roundedTotalExp)
+    if (job === currentJob && currentLevel === 1) {
+      return
     }
-  }, [totalExp])
+    const currentValues = {
+      exp: roundedTotalExp,
+      lv: 1,
+      job: job,
+      stats: currentStats,
+    }
+    setCurrentLevel(1)
+    setCurrentJob(job)
+    setBaseExp(roundedTotalExp)
+    setBaseLv(1)
+    setUnitHistory([...unitHistory, currentValues])
+    setStep(step + 1)
+  }
 
   return (
-    <div>
+    <div style={{ marginTop: 16 }}>
       <div>{unit}</div>
+      <div>{unitHistory.length}</div>
+      <div>EXP CAP: {expCap}</div>
+      <div>STEP: {step}</div>
       <div
         style={{
           display: 'flex',
@@ -96,8 +160,8 @@ export const AlphaSlider = ({ unit }: { unit: UnitName }) => {
           justifyContent: 'center',
         }}
       >
-        {unitHistory.map((history) => (
-          <div>
+        {unitHistory.map((history, index) => (
+          <div key={index}>
             {JOB_NAME[history.job]}
             {' -->'}
           </div>
@@ -112,8 +176,68 @@ export const AlphaSlider = ({ unit }: { unit: UnitName }) => {
           justifyContent: 'center',
         }}
       >
-        <button>Second Seal</button>
-        <button>Master Seal</button>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button>Second Seal</button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className='DropdownMenuContent'
+              sideOffset={5}
+            >
+              {isAdvanced &&
+                objectEntries(JOB_GROUP.ADVANCED).map(
+                  ([key, { isExclusive }]) => {
+                    if (!isExclusive || isExclusive === unit) {
+                      return (
+                        <DropdownMenu.Item
+                          key={key}
+                          className='DropdownMenuItem'
+                          textValue={key}
+                          onSelect={() => handleJobChange(key)}
+                        >
+                          {JOB_NAME[key]}
+                        </DropdownMenu.Item>
+                      )
+                    }
+                  }
+                )}
+              {objectEntries(JOB_GROUP.BASE).map(([key, { isExclusive }]) => {
+                if (!isExclusive || isExclusive === unit) {
+                  return (
+                    <DropdownMenu.Item
+                      key={key}
+                      className='DropdownMenuItem'
+                      textValue={key}
+                      onSelect={() => handleJobChange(key)}
+                    >
+                      {JOB_NAME[key]}
+                    </DropdownMenu.Item>
+                  )
+                }
+              })}
+              {objectEntries(JOB_GROUP.SPECIAL).map(
+                ([key, { isExclusive }]) => {
+                  if (!isExclusive || isExclusive === unit) {
+                    return (
+                      <DropdownMenu.Item
+                        key={key}
+                        className='DropdownMenuItem'
+                        textValue={key}
+                        onSelect={() => handleJobChange(key)}
+                      >
+                        {JOB_NAME[key]}
+                      </DropdownMenu.Item>
+                    )
+                  }
+                }
+              )}
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+        <Dropdown options={masterSealOptions}>
+          <button>Master Seal</button>
+        </Dropdown>
       </div>
 
       <div>level: {currentLevel}</div>
@@ -122,13 +246,12 @@ export const AlphaSlider = ({ unit }: { unit: UnitName }) => {
         style={{
           display: 'flex',
           flexDirection: 'column',
-          marginTop: 16,
           fontSize: 13,
         }}
       >
         {objectEntries(currentStats).map(([stat, value]) => (
-          <div>
-            {stat}: {getModifiedStat(stat, value)}
+          <div key={stat}>
+            {stat}: {value}
           </div>
         ))}
       </div>
@@ -136,7 +259,7 @@ export const AlphaSlider = ({ unit }: { unit: UnitName }) => {
       <SliderPrimitive.Slider
         min={0}
         max={8000}
-        value={[baseExp, totalExp]}
+        value={[EXP, totalExp]}
         onValueChange={onValueChange}
         className={'slider-root'}
       >
