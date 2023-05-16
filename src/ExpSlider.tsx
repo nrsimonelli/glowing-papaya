1fddd9556dc56f2ba8b256d158474f86a5034cb2
nrsimonelli <nrsimonelli@gmail.com>
2023-05-16 11:51:15 -0500

    feat(SLIDER): child component refresh

--- src/ExpSlider.tsx
+++ src/ExpSlider.tsx
@@ -2,11 +2,12 @@ import { Slider } from './components/Slider'
 import { Image } from './components/Image'
 import { JobChangeDropdown } from './JobChangeDropdown'
 import { JobName, UnitName, objectKeys } from './utils/types'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { UnitData } from './TeamPlanner'
 import { JOB_GROUP } from './constants'
 import { MinusCircledIcon, PlusCircledIcon } from '@radix-ui/react-icons'
 import { getDisplayName } from './utils/getDisplayName'
+import { isEqual } from 'lodash'
 
 interface ExpSliderProps {
   unit: UnitName
@@ -30,14 +31,21 @@ export const ExpSlider = ({
   removeUnitData,
   addUnitData,
 }: ExpSliderProps) => {
-  const { EXP, JOB, LV } = unitData[0]
-  const [baseExp, setBaseExp] = useState(EXP)
-  const [baseLv, setBaseLv] = useState(LV)
+  const { EXP, JOB, LV } = unitData[unitData.length - 1]
+  const [baseExp, setBaseExp] = useState(
+    unitData.length > 1
+      ? Math.floor(EXP / 100) * 100 - (LV - 1) * 100
+      : base.EXP
+  )
+  const [baseLv, setBaseLv] = useState(unitData.length > 1 ? 1 : LV)
   const [currentExp, setCurrentExp] = useState(EXP)
   const [currentJob, setCurrentJob] = useState<JobName>(JOB)
   const [currentLevel, setCurrentLevel] = useState(LV)
 
-  const [sliderValues, setSliderValues] = useState([base.EXP, EXP])
+  const [sliderValues, setSliderValues] = useState([
+    base.EXP,
+    ...unitData.map((x) => x.EXP),
+  ])
 
   const isSpecial = objectKeys(JOB_GROUP.SPECIAL).some(
     (job) => job === currentJob
@@ -66,7 +74,6 @@ export const ExpSlider = ({
   }
 
   const handleJobChange = (selectedJob: JobName) => {
-    // console.log(sliderValues)
     const sliderCopy = [...sliderValues]
     sliderCopy.splice(
       sliderValues.length - 1,
@@ -96,6 +103,22 @@ export const ExpSlider = ({
     removeUnitData(unit)
   }
 
+  useEffect(() => {
+    if (!isEqual(sliderValues, [base.EXP, ...unitData.map((x) => x.EXP)])) {
+      const data = unitData[unitData.length - 1]
+      const parentBase =
+        unitData.length > 1
+          ? Math.floor(EXP / 100) * 100 - (LV - 1) * 100
+          : base.EXP
+      setBaseExp(parentBase)
+      setBaseLv(unitData.length > 1 ? 1 : base.LV)
+      setCurrentExp(data.EXP)
+      setCurrentJob(data.JOB)
+      setCurrentLevel(data.LV)
+      setSliderValues([base.EXP, ...unitData.map((x) => x.EXP)])
+    }
+  }, [unitData])
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
       <div
@@ -108,7 +131,7 @@ export const ExpSlider = ({
         }}
       >
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <div>LV: {Math.floor((currentExp - baseExp) / 100 + baseLv)}</div>
+          <div>LV: {currentLevel}</div>
 
           <div
             style={{
@@ -155,7 +178,7 @@ export const ExpSlider = ({
           alignItems: 'center',
         }}
       >
-        <div style={{ flex: '1 0 120px' }}>
+        <div style={{ flex: '1 0 140px' }}>
           {getDisplayName(unit)}, {getDisplayName(currentJob)}
         </div>
         <Slider
