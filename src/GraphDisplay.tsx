336a218aa02e34f53f13dd7179b2f32053cabcef
nrsimonelli <nrsimonelli@gmail.com>
2023-05-16 11:51:52 -0500

    feat(VIS): functional color scheme

--- src/GraphDisplay.tsx
+++ src/GraphDisplay.tsx
@@ -1,5 +1,15 @@
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
-import { InitialUnitData, InitialStatData } from './TeamPlanner'
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { InitialUnitData, InitialStatData, StatName } from './TeamPlanner'
+import { UNIT_NAME } from './constants'
+import { UnitName } from './utils/types'
 
 export const GraphDisplay = ({
   unitData,
@@ -36,16 +46,28 @@ export const GraphDisplay = ({
     return data.totalExp - data.baseExp
   }
 
+  // const colorList = [
+  //   '#001D70',
+  //   '#00318A',
+  //   '#0047A5',
+  //   '#085EC0',
+  //   '#3D76DD',
+  //   '#5B8FF9',
+  //   '#7DAAFF',
+  //   '#9AC5FF',
+  //   '#B8E1FF',
+  // ]
+
   const colorList = [
-    '#001D70',
-    '#00318A',
-    '#0047A5',
-    '#085EC0',
-    '#3D76DD',
-    '#5B8FF9',
-    '#7DAAFF',
-    '#9AC5FF',
-    '#B8E1FF',
+    '#209FED',
+    '#1B6BED',
+    '#1444ED',
+    '#A0BFFF',
+    '#8881FF',
+    '#6752FF',
+    '#D7D1FF',
+    '#B78DFF',
+    '#8B59FF',
   ]
 
   const getColor = (index: number) => {
@@ -73,42 +95,60 @@ export const GraphDisplay = ({
     <ResponsiveContainer width='100%' height={350}>
       <BarChart data={data}>
         <XAxis
-          dataKey='id'
+          dataKey={'id'}
+          tickFormatter={(value) => UNIT_NAME[value as UnitName]}
           stroke='#888888'
-          fontSize={12}
+          fontSize={13}
           tickLine={false}
           axisLine={false}
         />
         <YAxis
           yAxisId={'stats'}
           stroke='#888888'
-          fontSize={12}
+          fontSize={13}
           tickLine={false}
           axisLine={false}
-          // tickFormatter={(value) => `$${value}`}
         />
         <YAxis
           yAxisId={'exp'}
           orientation='right'
           domain={[0, 6000]}
           stroke='#888888'
-          fontSize={12}
+          fontSize={13}
           tickLine={false}
           axisLine={false}
-          // tickFormatter={(value) => `$${value}`}
+        />
+        <Tooltip
+          contentStyle={{
+            background: '#2a2a2a',
+            fontSize: 13,
+            lineHeight: 1.5,
+            textAlign: 'right',
+            border: 'transparent',
+          }}
+          cursor={false}
+          formatter={(value) => Math.floor(Number(value))}
+        />
+        <Legend
+          align={'center'}
+          verticalAlign={'bottom'}
+          layout={'horizontal'}
         />
         {statData.map(({ id }, index) => (
           <Bar
+            key={`${id}-bar`}
             yAxisId={'stats'}
             dataKey={id}
+            name={StatName[id].label}
             stackId={'a'}
-            fill={getColor(index)}
+            fill={StatName[id].color}
             radius={index === statData.length - 1 ? [4, 4, 0, 0] : [0, 0, 0, 0]}
           />
         ))}
         <Bar
           yAxisId={'exp'}
           dataKey='baseExp'
+          name={'Base Exp'}
           stackId={'b'}
           fill='#65789B'
           radius={[0, 0, 0, 0]}
@@ -116,6 +156,7 @@ export const GraphDisplay = ({
         <Bar
           yAxisId={'exp'}
           dataKey={getExpDiff}
+          name={'Invested Exp'}
           stackId={'b'}
           fill='#61DDAA'
           radius={[4, 4, 0, 0]}
